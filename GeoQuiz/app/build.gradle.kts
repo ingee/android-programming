5a2ace4624c23d1d08e09e0aef5aa0f539b65dd9
InGee Kim <ingee.kim@sk.com>
2025-03-11 10:20:38 +0900

    Ch6/GeoQuiz: Write UI tests for MainActivity using Espresso

--- GeoQuiz/app/build.gradle.kts
+++ GeoQuiz/app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     testImplementation(libs.junit)
     testImplementation(libs.junit.junit)
+    testImplementation(libs.junit.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
