a428390b665a884ab81bc2424cf18efef3c3ac12
Kim InGee <ingee.kim@gmail.com>
2025-08-11 14:12:18 +0900

    Ch20/PhotoGallery: Add PhotoGalleryViewModel to handle Flickr API requests

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.bignerdranch.android.photogallery.databinding.FragmentPhotoGalleryBinding
@@ -19,6 +20,8 @@ class PhotoGalleryFragment : Fragment() {
         get() = checkNotNull(_binding) {
             "Cannot access binding because it is null. Is the view visible?"
         }
+
+    private val photoGalleryViewModel: PhotoGalleryViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -39,8 +42,9 @@ class PhotoGalleryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewLifecycleOwner.lifecycleScope.launch {
-            val response = PhotoRepository().fetchPhotos()
-            Log.d(TAG, "Response received: $response")
+            photoGalleryViewModel.galleryItems.collect { items ->
+                Log.d(TAG, "Response received: $items")
+            }
         }
     }
 }
