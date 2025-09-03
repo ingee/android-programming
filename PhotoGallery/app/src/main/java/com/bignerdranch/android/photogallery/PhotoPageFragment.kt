0f8734cde94a7783ada1e466b5a59ca255993e7e
Kim InGee <ingee.kim@gmail.com>
2025-09-03 14:23:15 +0900

    Ch23/PhotoGallery: Add progress-bar to show WebView loading progress

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoPageFragment.kt
@@ -1,13 +1,19 @@
 package com.bignerdranch.android.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.bignerdranch.android.photogallery.databinding.FragmentPhotoPageBinding
+
+private const val TAG = "PhotoPageFragment"
 
 class PhotoPageFragment : Fragment() {
     private val args: PhotoPageFragmentArgs by navArgs()
@@ -24,10 +30,29 @@ class PhotoPageFragment : Fragment() {
         )
 
         binding.apply {
+            progressBar.max = 100
+
             webView.apply {
                 settings.javaScriptEnabled = true
                 webViewClient = WebViewClient()
+                Log.d(TAG, "about to load ${args.photoPageUri.toString()}")
                 loadUrl(args.photoPageUri.toString())
+
+                webChromeClient = object : WebChromeClient() {
+                    override fun onProgressChanged(view: WebView?, newProgress: Int) {
+                        if (newProgress == 100) {
+                            binding.progressBar.visibility = View.GONE
+                        } else {
+                            binding.progressBar.visibility = View.VISIBLE
+                            binding.progressBar.progress = newProgress
+                        }
+                    }
+
+                    override fun onReceivedTitle(view: WebView?, title: String?) {
+                        val parent = requireActivity() as AppCompatActivity
+                        parent.supportActionBar?.subtitle = title
+                    }
+                }
             }
         }
         return binding.root
