6ee58ef958b2e6bebfbbe3e998fd822f83fb4f41
Kim InGee <ingee.kim@gmail.com>
2025-09-16 10:24:44 +0900

    Ch24/DragDraw: Track touch movement

--- DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.kt
+++ DragAndDraw/app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.kt
@@ -13,24 +13,42 @@ class BoxDrawingView(
     context: Context,
     attrs: AttributeSet? = null
 ) : View(context, attrs) {
+
+    private var currentBox: Box? = null
+    private val boxes = mutableListOf<Box>()
+
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         val current = PointF(event!!.x, event!!.y)
         var action = ""
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
                 action = "ACTION_DOWN"
+                currentBox = Box(current).also {
+                    boxes.add(it)
+                }
             }
             MotionEvent.ACTION_MOVE -> {
                 action = "ACTION_MOVE"
+                updateCurrentBox(current)
             }
             MotionEvent.ACTION_UP -> {
                 action = "ACTION_UP"
+                updateCurrentBox(current)
+                currentBox = null
             }
             MotionEvent.ACTION_CANCEL -> {
                 action = "ACTION_CANCEL"
+                currentBox = null
             }
         }
         Log.i(TAG, "$action at x=${current.x}, y=${current.y}")
         return true
+    }
+
+    private fun updateCurrentBox(current: PointF) {
+        currentBox?.let {
+            it.end = current
+            invalidate()
+        }
     }
 }
