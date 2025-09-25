adbf4e4f347a6b3f140e16e74f8e0b02041e57a8
Kim InGee <ingee.kim@gmail.com>
2025-09-25 16:36:33 +0900

    Ch25/Sunset: Add color animation to Sky view

--- Sunset/app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
+++ Sunset/app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.bignerdranch.android.sunset
 
+import android.animation.ArgbEvaluator
 import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.animation.AccelerateInterpolator
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.bignerdranch.android.sunset.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val blueSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.blue_sky)
+    }
+    private val sunsetSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.sunset_sky)
+    }
+    private val nightSkyColor: Int by lazy {
+        ContextCompat.getColor(this, R.color.night_sky)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +42,14 @@ class MainActivity : AppCompatActivity() {
         val heightAnimator = ObjectAnimator
             .ofFloat(binding.sun, "y", sunYStart, sunYEnd)
             .setDuration(3000)
+        heightAnimator.interpolator = AccelerateInterpolator()
+
+        val sunsetSkyAnimator = ObjectAnimator
+            .ofInt(binding.sky, "backgroundColor", blueSkyColor, sunsetSkyColor)
+            .setDuration(3000)
+        sunsetSkyAnimator.setEvaluator(ArgbEvaluator())
+
         heightAnimator.start()
+        sunsetSkyAnimator.start()
     }
 }
