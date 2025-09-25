f5fc7f0c9a4264a0e047424e282a3bbcaa119e5b
Kim InGee <ingee.kim@gmail.com>
2025-09-25 10:14:28 +0900

    Ch25/Sunset: Add downward animation to Sun view on screen click

--- Sunset/app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
+++ Sunset/app/src/main/java/com/bignerdranch/android/sunset/MainActivity.kt
@@ -1,20 +1,34 @@
 package com.bignerdranch.android.sunset
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.bignerdranch.android.sunset.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.scene)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.scene.setOnClickListener {
+            startAnimation()
         }
+    }
+
+    private fun startAnimation() {
+        val sunYStart = binding.sun.top.toFloat()
+        val sunYEnd = binding.sky.height.toFloat()
+
+        val heightAnimator = ObjectAnimator
+            .ofFloat(binding.sun, "y", sunYStart, sunYEnd)
+            .setDuration(3000)
+        heightAnimator.start()
     }
 }
