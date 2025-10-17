403ad851c5347f9cd21c2e6060f4c853b7cca8e7
Kim InGee <ingee.kim@gmail.com>
2025-10-17 13:23:03 +0900

    Ch27/CodaPizza: Change global variable 'pizza' as remember variable

--- CodaPizza/app/src/main/java/com/bignerdranch/android/codapizza/ui/PizzaBuilderScreen.kt
+++ CodaPizza/app/src/main/java/com/bignerdranch/android/codapizza/ui/PizzaBuilderScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -25,8 +26,6 @@ import com.bignerdranch.android.codapizza.R
 import com.bignerdranch.android.codapizza.model.Pizza
 import com.bignerdranch.android.codapizza.model.Topping
 import com.bignerdranch.android.codapizza.model.ToppingPlacement
-
-private var pizza by mutableStateOf(Pizza())
 
 @Preview
 @Composable
@@ -53,6 +52,8 @@ fun PizzaBuilderScreen(
 private fun ToppingList(
     modifier: Modifier = Modifier
 ) {
+    var pizza by remember { mutableStateOf(Pizza()) }
+
     LazyColumn(
         modifier = modifier
     ) {
