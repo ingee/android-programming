56c910de09f96b60dc642378d516b76cdfd1b749
Kim InGee <ingee.kim@gmail.com>
2025-10-29 11:48:23 +0900

    Ch29/CodaPizza: Customizing the Image composable

--- CodaPizza/app/src/main/java/com/bignerdranch/android/codapizza/ui/PizzaHeroImage.kt
+++ CodaPizza/app/src/main/java/com/bignerdranch/android/codapizza/ui/PizzaHeroImage.kt
@@ -3,15 +3,21 @@ package com.bignerdranch.android.codapizza.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.bignerdranch.android.codapizza.R
 import com.bignerdranch.android.codapizza.model.Pizza
 import com.bignerdranch.android.codapizza.model.Topping
-import com.bignerdranch.android.codapizza.model.ToppingPlacement
+import com.bignerdranch.android.codapizza.model.ToppingPlacement.All
+import com.bignerdranch.android.codapizza.model.ToppingPlacement.Left
+import com.bignerdranch.android.codapizza.model.ToppingPlacement.Right
 
 @Preview
 @Composable
@@ -19,9 +25,9 @@ private fun PizzaHeroImagePreview() {
     PizzaHeroImage(
         pizza = Pizza(
             toppings =  mapOf(
-                Topping.Pineapple to ToppingPlacement.All,
-                Topping.Pepperoni to ToppingPlacement.Left,
-                Topping.Basil to ToppingPlacement.Right
+                Topping.Pineapple to All,
+                Topping.Pepperoni to Left,
+                Topping.Basil to Right
             )
         )
     )
@@ -34,17 +40,34 @@ fun PizzaHeroImage(
 ) {
     Box(
         modifier = modifier
+            .aspectRatio(1f)
     ) {
         Image(
             painter = painterResource(R.drawable.pizza_crust),
             contentDescription = stringResource(R.string.pizza_preview),
+            modifier = Modifier.fillMaxSize()
         )
 
         pizza.toppings.forEach { (topping, placement) ->
             Image(
                 painter = painterResource(topping.pizzaOverlayImage),
                 contentDescription = null,
+                contentScale = ContentScale.Crop,
+                alignment = when(placement) {
+                    Left -> Alignment.TopStart
+                    Right -> Alignment.TopEnd
+                    All -> Alignment.Center
+                },
                 modifier = Modifier.focusable(false)
+                    .aspectRatio(when(placement) {
+                        Left, Right -> 0.5f
+                        All -> 1.0f
+                    })
+                    .align(when(placement) {
+                        Left -> Alignment.CenterStart
+                        Right -> Alignment.CenterEnd
+                        All -> Alignment.Center
+                    })
             )
         }
     }
