c9ee31d88db5fcebd7e374f068f689947cd7f838
Kim InGee <ingee.kim@gmail.com>
2025-10-29 11:52:35 +0900

    Ch29/CodaPizza: Adding a header to LazyColumn

--- CodaPizza/app/src/main/java/com/bignerdranch/android/codapizza/ui/PizzaBuilderScreen.kt
+++ CodaPizza/app/src/main/java/com/bignerdranch/android/codapizza/ui/PizzaBuilderScreen.kt
@@ -73,6 +73,13 @@ private fun ToppingList(
     LazyColumn(
         modifier = modifier
     ) {
+        item {
+            PizzaHeroImage(
+                pizza = pizza,
+                modifier = Modifier.padding(16.dp)
+            )
+        }
+
         items(Topping.values()) { topping ->
             ToppingCell(
                 topping = topping,
